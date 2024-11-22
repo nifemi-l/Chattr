bb8e3b28b5eb04705679c1920f9496c75fa03ae6
Nifemi Lawal <143533298+nifemi-l@users.noreply.github.com>
2024-11-21 18:42:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ db.serialize(() => {
 // api routes
 
 // new user registration
-app.post("register", async (req, res) => {
+app.post("/register", async (req, res) => {
     const { username, password } = req.body; // get user and pw
     const hashedPassword = await bcrypt(password, 10);
     // insert into db
